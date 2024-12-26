674895b88b6560885f5f0b7c4d67f7ff7e800c68
14kerry <165141981+14kerry@users.noreply.github.com>
2024-12-26 13:46:32 +0300

    cofig solution

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-    app_id: '64225',
-token: 'kOIH9bYFuNuWKU3',
+    app_id: '66771',
+      
     oauth: {
         redirect_uri: 'https://playful-naiad-8d08a1.netlify.app/redirect.html',
         post_login_redirect: 'https://playful-naiad-8d08a1.netlify.app/index.html',
