e61d1a334fb53ccbda3beff6e5748fce359cf554
14kerry <165141981+14kerry@users.noreply.github.com>
2024-12-26 11:23:18 +0300

    cofig sori

--- config.js
+++ config.js
@@ -1,16 +1,9 @@
 const config = {
-    app_id: process.env.DERIV_APP_ID || 'YOUR_DERIV_APP_ID',
-    api_token: process.env.DERIV_API_TOKEN || 'YOUR_DERIV_API_TOKEN',
-    base_path: location.hostname === 'localhost' ? '' : '/New-folder',
-    oauth: {
-        redirect_uri: location.hostname === 'localhost' 
-            ? window.location.origin + '/redirect.html'
-            : window.location.origin + '/New-folder/redirect.html',
-        post_login_redirect: location.hostname === 'localhost'
-            ? '/index.html'
-            : '/New-folder/index.html',
-        post_logout_redirect: location.hostname === 'localhost'
-            ? '/login.html'
-            : '/New-folder/login.html'
-    }
+    app_id: '1089',
+token: 'kOIH9bYFuNuWKU3',
+oauth: {
+    redirect_uri: 'https://14kerry.github.io/K_E_bot/redirect.html',
+    post_login_redirect: 'https://14kerry.github.io/K_E_bot/index.html',
+    post_logout_redirect: 'https://14kerry.github.io/K_E_bot/login.html'
+}
 }; 
